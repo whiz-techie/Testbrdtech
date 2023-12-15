d0458daa088b0585be7ef88de8c5267fcde01ab3
whiz-techie <codeminded365@gmail.com>
2023-12-15 20:36:32 +0500

    goals cards updated

--- src/components/Home.js
+++ src/components/Home.js
@@ -6,16 +6,16 @@ import Goals from './Sections/Goals'
 
 const Home = () => {
   return (
-    <div className='flex flex-col justify-around md:justify-between h-[550vh] sm:h-[500vh] md:h-[450vh]'>
+    <div className='flex flex-col justify-around md:justify-between h-[650vh] sm:h-[200%] md:h-[200%]'>
         <Banner/>
       <span className=''>
         
         <Cards/>
       </span>
-        <div>
+        {/* <div> */}
 
         <Goals />
-        </div>
+        {/* </div> */}
         <div className='flex w-full justify-center mt-[40px]'>
         {/* <div className=' grid grid-cols-5 '> */}
           {/* <div></div> */}
